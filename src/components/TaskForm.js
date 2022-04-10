15449dadf2d4bec20b63c727cbe1f74a565021b4
Jose Campo Rivas <josecampor31@gmail.com>
2022-04-10 14:41:13 +0200

    improving component TaskForm

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,13 +1,15 @@
 import React from "react";
 //import plusPng from "../images/plus.png"
-import plusSvg from "../images/svg/bx-plus.svg"
+import plusSvg from "../images/svg/bx-plus.svg";
+import '../styles/taskForm.css'
 
 export default class TaskForm extends React.Component{
     render(){
         return (
-            <div>
-                TaskForm
-                <button style={{backgroundColor: 'red'}} ><img src={plusSvg}/></button>
+            <div className="taskForm">
+                <label id="labelTaskName" htmlFor="taskName">Add a new task</label>
+                <input id="taskName" type="text"/>
+                <button id="buttonTaskSubmit"><img src={plusSvg}/></button>
             </div>
         )
     }
